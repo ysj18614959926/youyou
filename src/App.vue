8c99ffa715b216acc685fe2eace146e9c8a7c94a
yangshujing <yangshujing_v@didichuxing.com>
2023-03-14 15:49:33 +0800

    环境配置

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
+  <div class="head"></div>
   <router-view></router-view>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+body {
+  margin: 0;
+  background-color: #f1f5f9;
+  .head {
+    height: 60px;
+    background-color: #243049;
+  }
 }
 </style>
